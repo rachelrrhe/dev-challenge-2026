4a05ce99113f7feb584bf2c41feb54ec43e1598d
Rachel He <rachelhe_08@126.com>
2026-09-08 13:57:35 -0700

    A3 restricted address and rating

--- client/app/api/restaurants/route.ts
+++ client/app/api/restaurants/route.ts
@@ -35,11 +35,17 @@ export async function POST(_req: Request) {
   try {
     // Read the request body
     const body = await _req.json();
+    // I restricted the address format to start with a number and then a string like "301 Olive Ave" in the example.
+    const ADDRESS_PATTERN = /^[0-9][a-zA-Z\s.,'#-]{4,99}$/;
 
-    const { name, location, rating } = body;
+    const { name, cuisine, address, rating } = body;
+    // I also restricted the rating to be between 0-5.
+    if (!name || !cuisine || !address || !ADDRESS_PATTERN.test(address) || !rating || rating < 0 || rating > 5) {
+      return NextResponse.json({ error: 'Invalid request body' }, { status: 400 });
+    }
     const { rows } = await pool.query(
-      'INSERT INTO restaurants (name, location, rating) VALUES ($1, $2, $3) RETURNING *',
-      [name, location, rating]
+      'INSERT INTO restaurants (name, cuisine, address, rating) VALUES ($1, $2, $3, $4) RETURNING *',
+      [name, cuisine, address, rating]
     );
 
     return NextResponse.json(toRestaurant(rows[0]), { status: 201 });
